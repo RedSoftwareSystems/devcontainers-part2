0869da51b2051765d3d6c6f68072d37be0146913
Andrea Chiumenti <achiumenti@red.software.systems>
2026-04-29 22:40:04 +0200

    package updates

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist/**', 'node_modules/**'] },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+  },
+);
